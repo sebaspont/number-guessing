1f10cca32e45bae76e5e118352a0c2db75e72c0c
Sebastian Pont Verges <sebaspont@gmail.com>
2024-03-22 18:22:38 +0100

    Created functions generateTarget and compareGuesses

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+// function generateTarget:
 function generateTarget() {
   return Math.floor(Math.random()*10);
 }
 
+// function compareGuesses
 function compareGuesses(humanGuess, computerGuess, targetNumber) {
   if (humanGuess.Math.abs() >= computerGuess.Math.abs()) {
     return true;
