9164418290ed746b6828f986077d37c33d20f133
Sebastian Pont Verges <sebaspont@gmail.com>
2024-03-23 11:55:31 +0100

    Turned generateTarget function declaration into const generateTarget with arrow function

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // function generateTarget:
-function generateTarget() {
+const generateTarget = () => {
   return Math.floor(Math.random()*10);
 }
 
 // console.log(generateTarget())
 
-// // function compareGuesses
-// function compareGuesses(humanGuess, computerGuess, targetNumber) {
-//   if (humanGuess.Math.abs() >= computerGuess.Math.abs()) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
+// function compareGuesses
+function compareGuesses(humanGuess, computerGuess, generateTarget) {
+  if (humanGuess.Math.abs() >= computerGuess.Math.abs()) {
+    return true;
+  } else {
+    return false;
+  }
+}
