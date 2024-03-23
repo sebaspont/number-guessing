090e0dce504909f889a660aee25148a1eb7391a5
Sebastian Pont Verges <sebaspont@gmail.com>
2024-03-23 12:44:40 +0100

    Created function updateScore

--- script.js
+++ script.js
@@ -24,3 +24,22 @@ const compareGuesses = (humanGuess, computerGuess, secretTarget) => {
   // concise option
   return humanDiff >= computerDiff;
 }
+
+// Create an updateScore() function. This function:
+
+// Has a single parameter. This parameter will be a string value representing the winner.
+// Increases the score variable (humanScore or computerScore) by 1 depending on the winner passed in to updateScore.
+// The string passed in will be either 'human' or 'computer'.
+// Does not need to return any value.
+// The purpose of this function is to be used to correctly increase the winner’s score after each round.
+
+// updateScore function
+// since it takes only one parameter, the parenthesis around winner can be ommited!
+const updateScore = (winner) => {
+  if (winner === 'human') {
+    // += 1 can also be written as ++;
+    humanScore += 1;
+  } else {
+    computerScore += 1;
+  }
+}
